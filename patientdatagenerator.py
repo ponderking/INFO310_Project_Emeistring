c9a0656297b56054e213d1dbdbea98ea8d33f6df
Martin Torvanger <martin_torvanger@hotmail.com>
2020-04-24 18:28:02 +0200

    Add files via upload

--- patientdatagenerator.py
+++ patientdatagenerator.py
@@ -1,34 +1,52 @@
-from rdflib import Graph, Literal, Namespace, URIRef
-from rdflib.namespace import RDF, FOAF, RDFS, OWL
-import pandas as pd
+import json
+import random
+import pprint
 
-g = Graph()
-ex = Namespace("http://example.org/")
-g.bind("ex", ex)
+modules_amount = 14
+patients_amount = 15
 
-# Load the CSV data as a pandas Dataframe.
-csv_data = pd.read_csv("task1.csv")
+tasks_categories = ["Family_or_Friends", "Health", "Free_Time", "Work_or_Study"]
+tasks = ["Fishing", "Gardening", "Indoor Games", "Job",
+         "Outdoor Games", "Reading", "Shopping", "Sleeping", "Tour", "Exercise", "Writing"]
 
-# Here I deal with spaces (" ") in the data. I replace them with "_" so that URI's become valid.
-csv_data = csv_data.replace(to_replace=" ", value="_", regex=True)
 
-# Here I mark all missing/empty data as "unknown". This makes it easy to delete triples containing this later.
-csv_data = csv_data.fillna("unknown")
+json_data = {}
+json_data['modules'] = []
 
-# Loop through the CSV data, and then make RDF triples.
-for index, row in csv_data.iterrows():
-    # The names of the people act as subjects.
-    subject = row['Name']
-    # Create triples: e.g. "Cade_Tracey - age - 27"
-    g.add((URIRef(ex + subject), URIRef(ex + "age"), Literal(row["Age"])))
-    g.add((URIRef(ex + subject), URIRef(ex + "married"), URIRef(ex + row["Spouse"])))
-    g.add((URIRef(ex + subject), URIRef(ex + "country"), URIRef(ex + row["Country"])))
+i = 1
+j = 1
+while i <= modules_amount:
+    patients = []
+    j = 1
+    while j <= patients_amount:
+        patient_active_chance = random.randrange(1, 10)
+        therapist_active_chance = random.randrange(1, 10)
 
-    # If We want can add additional RDF/RDFS/OWL information e.g
-    g.add((URIRef(ex + subject), RDF.type, FOAF.Person))
+        completion = True
+        if patient_active_chance == 1:
+            completion = False
+        therapist_active = True
+        if therapist_active_chance == 1:
+            therapist_active = False
 
-# I remove triples that I marked as unknown earlier.
-g.remove((None, None, URIRef("http://example.org/unknown")))
+        patients.append({
+            "name": "patient_" + str(j),
+            "completion": str(completion),
+            "activity_category": random.choice(tasks_categories),
+            "activity": random.choice(tasks),
+            "therapist_active": str(therapist_active),
+            "madris_score": random.randrange(0, 60),
+            })
+        j += 1
 
-# Clean printing of the graph.
-print(g.serialize(format="turtle").decode())
+    json_data['modules'].append(
+        {
+            "module_" + str(i): {"patients": patients},
+        }
+    )
+    i += 1
+
+pprint.pprint(json_data)
+
+with open('data/patient_data.json', 'w') as outfile:
+    json.dump(json_data, outfile)
